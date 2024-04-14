be08cf9f95a60d6f4345b02f613a5dae3e59348b
VladLen43 <136459377+VladLen43@users.noreply.github.com>
2024-04-14 17:45:10 +0300

    editingform

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,21 +1,27 @@
 import React, { useEffect } from 'react'
 import styles from './Profile.module.scss'
 import { Button } from '@mui/material'
-import { Link } from 'react-router-dom'
+import { Link, Location, useLocation, useNavigate } from 'react-router-dom'
 import { useAppSelector } from '../../redux/hooks'
 import { fetchAuthMe, isAuth } from '../../redux/reducers/auth'
+import { Header } from '../../components/Header/Header'
 
 export const Profile = () => {
     useEffect(() => {
         fetchAuthMe()
     },[])
-    
+
+        
+    const location = useNavigate() 
     const user = useAppSelector((state) => state.auth.data)
+ 
+
 
   return (
     <div className={styles.container}>
+        <Header/>
+
         <h1>Профиль</h1>
-        <Button ><Link to="/">На главную</Link></Button>
         {/* @ts-ignore */}
         <div>Имя: {user?.fullName}</div>
         {/* @ts-ignore */}
